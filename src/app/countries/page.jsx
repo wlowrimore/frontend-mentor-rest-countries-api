54b9fd255cd1d008fd2036f15d085715c56d9664
William Lowrimore <wlowrimore@gmail.com>
2023-11-30 00:45:17 -0600

    adds pagination to countries page

--- src/app/countries/page.jsx
+++ src/app/countries/page.jsx
@@ -10,6 +10,12 @@ const Countries = ({ query, setIsOpen }) => {
   const [countries, setCountries] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState('');
 
+  // Pagination Variables
+  const [currentPage, setCurrentPage] = useState(1);
+  const countriesPerPage = 10;
+  const startIndex = (currentPage - 1) * countriesPerPage;
+  const endIndex = startIndex + countriesPerPage;
+
   const API_URL = selectedRegion
     ? `https://restcountries.com/v3.1/region/${selectedRegion}`
     : 'https://restcountries.com/v3.1/all'
@@ -36,9 +42,38 @@ const Countries = ({ query, setIsOpen }) => {
     getCountriesData();
   }, [API_URL]);
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  }
+
   const handleRegionSelect = (region) => {
     setSelectedRegion(region);
   }
+
+  const renderPageNumbers = () => {
+    const pageCount = Math.ceil(countries.length / countriesPerPage);
+    const pageNumbers = [];
+    const numSpread = <span className="text-gray-300 text-2xl">. . .</span>;
+
+    for (let i = 1; i <= pageCount; i++) {
+      if (i === 1 || i === pageCount || (i >= currentPage - 2 && i <= currentPage + 2)) {
+        pageNumbers.push(
+          <button
+            key={i}
+            className={`px-2 pb-1 hover:text-orange-300 ${currentPage === i ? 'underline text-orange-400' : 'text-gray-200'
+              }`}
+            onClick={() => handlePageChange(i)}
+          >
+            {i}
+          </button>
+        );
+      } else if (i === currentPage - 3 || i === currentPage + 3) {
+        pageNumbers.push(numSpread);
+      }
+    }
+
+    return pageNumbers;
+  };
 
   return (
     <>
@@ -47,7 +82,7 @@ const Countries = ({ query, setIsOpen }) => {
         <DropdownForRegion onRegionSelect={handleRegionSelect} />
       </div>
       <div className='flex flex-wrap w-full mt-28 mb-16 md:mt-16 justify-center gap-10 2xl:grid grid-cols-5'>
-        {countries && countries.map((country) => (
+        {countries.slice(startIndex, endIndex).map((country) => (
           <Link href={`/countries/${country.cca2}`} key={country.cca2} className='w-[285px] hover:opacity-60 transform duration-200 ease-in'>
             <div className='flex flex-col'>
               <Image
@@ -65,6 +100,30 @@ const Countries = ({ query, setIsOpen }) => {
             </div>
           </Link>
         ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          className={`px-2 pb-1 hover:text-orange-400 ${currentPage === 1 ? 'text-gray-200' : 'text-gray-200'}`}
+          onClick={() => handlePageChange(1)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+
+
+
+        </button>
+        {renderPageNumbers()}
+        <button
+          className={`px-2 pb-1 hover:text-orange-400 ${currentPage === Math.ceil(countries.length / countriesPerPage) ? 'bg-gray-800 text-white' : 'text-gray-200'}`}
+          onClick={() => handlePageChange(Math.ceil(countries.length / countriesPerPage))}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+
+
+        </button>
       </div>
     </>
   );
